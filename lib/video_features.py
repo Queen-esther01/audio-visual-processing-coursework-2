99a462bad2eca5a4891e82c2abb14ebb5180b237
Becca-Saka <thebeccasaka@gmail.com>
2025-11-27 04:22:03 +0000

    feat: apply dct and model

---
+++ lib/video_features.py
@@ -0,0 +1,71 @@
+import numpy as np
+import cv2
+
+from lib.lip_extraction import LipExtraction
+
+class VideoFeatures:
+    def __init__(self):
+        self.lE = LipExtraction()
+
+    def apply_dct(self, s_image):
+        img = cv2.cvtColor(s_image, cv2.COLOR_BGR2GRAY)
+
+        img = cv2.resize(img, (64, 64))
+
+        img_float = np.float32(img)
+        img_dct = cv2.dct(img_float)
+        img_dct_log = np.log(np.abs(img_dct))
+
+        N = 32
+        img_dct_trunc = np.zeros_like(img_dct)
+        img_dct_trunc[:N, :N] = img_dct[:N, :N]
+        img_new = cv2.idct(img_dct_trunc)
+        features = img_new.flatten()
+        # Utils.draw_tmp(img, img_new)
+        return features
+
+    def apply_dct_21(s_image):
+        # s_image = lip ROI (BGR)
+        if s_image.ndim == 3:
+            img = cv2.cvtColor(s_image, cv2.COLOR_BGR2GRAY)
+        else:
+            img = s_image
+
+        img = cv2.resize(img, (32, 32))
+        img_float = np.float32(img)
+        img_dct = cv2.dct(img_float)
+
+        # take small block e.g. 5x5 = 25 coeffs, then keep 21
+        block = img_dct[:5, :5].flatten()  # length 25
+        feats = block[:21]  # length 21
+        return feats
+
+    def features_by_video(self, frames, name):
+        frame_feats = []
+        frame_index = 0
+        for frame in frames:
+            # if frame_index >0:
+            #     break
+            # frame_index += 1
+
+            s_roi = self.lE.get_roi(frame, name, draw_plot=False)
+            if s_roi is None:
+                continue
+
+            features = self.apply_dct(s_roi)
+            # print("features", len(features))
+            # data.append(features)
+
+            frame_feats.append(features)
+        if not frame_feats:
+            return None
+
+        frame_feats = np.array(frame_feats)
+        video_features = np.mean(frame_feats, axis=0)
+        return video_features
+
+    def get_features(self, path, name):
+
+        frames = self.lE.extract_frames(path)
+        video_features = self.features_by_video(frames, name)
+        return video_features
--- main.py
+++ main.py
@@ -1,87 +1,119 @@
 import os
 import glob
-
-import matplotlib.pyplot as plt
-import numpy as np
-import cv2
+from lib.linear_model import LinearModel
+from lib.visual_feature_interp import *
+from concurrent.futures import ProcessPoolExecutor
 
 # use cascasge to crop the lips
 # Use ffmeg to recompose the video and work on it.
 # then apply dct on a 20ms frames
-# compare the result with the audio and visual sector
-
-from lip_extraction import LipExtraction
-from utils import Utils
+# compare the results with the audio and visual sector
 
 
+base_path = "results"
+normalizer_name = f"{base_path}/norm_stats.npz"
+output_name = f"{base_path}/correct_predictions.png"
+
+
+def get_name(file):
+    filename = os.path.basename(file)
+
+    name, ext = os.path.splitext(filename)
+
+
+    name_split = name.split("_")
+    label = name_split[0].title()
+    label_number = int(name_split[1])
+    return name, label, label_number, filename
+
+
+# def get_features():
+#     data = []
+#     labels = []
+#     limit = 0
+#     last_name = ''
+#     for file in sorted(glob.glob('audio_video_files/*.MOV')):
+#         name, label, label_number, filename = get_name(file)
+#
+#         if last_name == label and label_number > 6:
+#             continue
+#         print("Processing:", name)
+#
+#         last_name = label
+#         video_features = vF.get_features(file, filename)
+#         # audio_features = aF.get_features(file, name)
+#         if video_features is None:
+#             # if video_features is None or audio_features is None:
+#             continue
+#         data.append(video_features)
+#
+#         labels.append(label)
+#     return data, labels
+
+
+def process_single_file(file):
+    from lib.video_features import VideoFeatures
+    from lib.audio_features import AudioFeatures
+    name, label, label_number, filename = get_name(file)
+    print("processing file:", file)
+
+    vf = VideoFeatures()
+    af = AudioFeatures()
+    video_features = vf.get_features(file, filename)
+    audio_features = af.get_features(file, filename)
+    combined_features = visual_feature_interp(video_features, audio_features)
+
+    if combined_features is None:
+        return None
+
+    return combined_features, label
+
+def get_features():
+    all_files = sorted(glob.glob('src/audio_video_files/*.MOV'))
+
+    selected_files = []
+    seen_per_label = {}
+
+    for file in all_files:
+        name, label, label_number, filename = get_name(file)
+
+        count = seen_per_label.get(label, 0)
+        if count >= 6:
+            continue
+
+        seen_per_label[label] = count + 1
+        selected_files.append(file)
+
+    print("Selected files:", len(selected_files))
+
+    data = []
+    labels = []
+
+    with ProcessPoolExecutor(max_workers=4) as executor:
+        for result in executor.map(process_single_file, selected_files):
+            if result is None:
+                continue
+            video_features, label = result
+            data.append(video_features)
+            labels.append(label)
+
+    return data, labels
 
 def main():
     print("Hello from audio-visual-processing-coursework-2!")
-    # extract_frames()
-    lE = LipExtraction()
-    data = []
-    limit = 0
-    for video_file in sorted(glob.glob('audio_video_files/*.MOV')):
-        filename = os.path.basename(video_file)
-        name, ext = os.path.splitext(filename)
-        print("Processing:", name)
+    data, labels = get_features()
+    from collections import Counter
 
-        if limit > 0:
-            break
-        limit += 1
+    print("Label counts:", Counter(labels))
+    data = np.array(data)
+    linear_model = LinearModel()
+    mean = np.mean(data, axis=0)
+    std = np.std(data, axis=0) + 1e-8
+    np.savez(normalizer_name, mean=mean, std=std)
+    data = (data - mean) / std
+    labels = linear_model.labelEncoder(labels)
+    linear_model.train_model(data, labels)
 
-        frames = lE.extract_frames(video_file)
-        frame_index = 0
-        for frame in frames:
-            if frame_index >0:
-                break
-            frame_index += 1
-
-            print("Test frame", frame)
-
-            s_roi = lE.get_roi(frame, name, draw_plot=False)
-
-            # features = apply_dct(s_roi)
-            # data.append(features)
-
-
-# def main():
-#     print("Hello from audio-visual-processing-coursework-2!")
-#     # extract_frames()
-#     lE = LipExtraction()
-#     data = []
-#     limit = 0
-#     for video_file in sorted(glob.glob('audio_video_files/*.MOV')):
-#         if limit > 0:
-#             break
-#         limit += 1
-#         filename = os.path.basename(video_file)
-#         name, ext = os.path.splitext(filename)
-#         frames = lE.extract_frames(video_file)
-#         data.append((frames, name))
-#     print("Data length", len(data))
-#     for i in range(len(data)):
-#
-#         test_frame, name = data[i]
-#         print("Test frame", test_frame)
-#         test_frame = test_frame[0]
-#         print("Processing:", name)
-#         s_roi = lE.get_roi(test_frame, name, draw_plot=False)
-#
-#         apply_dct(s_roi)
-#
-#     # data = extract_frames()
-#     # # test_frame, name = data[0]
-#     # # test_frame = test_frame[0]
-#     # # plt.imshow(cv2.cvtColor(test_frame, cv2.COLOR_BGR2RGB))
-#     # # plt.show()
-#     #
-#     # for i in range(len(data)):
-#     #     test_frame, name = data[i]
-#     #     test_frame = test_frame[0]
-#     #     print("Processing:", name)
-#     #     get_roi(test_frame, name)
-#     #     # apply_dct(test_frame)
 
 if __name__ == "__main__":
     main()
