25c04f072f8786f693d8c950606adf9242e9ea7c
Esther Ejidike <ejidikeesther@gmail.com>
2025-11-27 14:06:44 +0000

    updates to extract audio

--- extract_audio.py
+++ extract_audio.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import librosa.feature as lf
+import librosa
 import numpy as np
 import soundfile as sf
 from moviepy import VideoFileClip
@@ -14,15 +15,29 @@ def convert_video_to_audio_moviepy(video_file, output_ext="mp3"):
     filename, ext = os.path.splitext(video_file)
     filename = filename.split("/")[-1]
     clip = VideoFileClip(video_file)
-    clip.audio.write_audiofile(f"audio_files/{filename}.{output_ext}")
+    print("clip",clip)
+    # clip.audio.write_audiofile(f"audio_files/{filename}.{output_ext}")
+    data = clip.audio.to_soundarray()
+    print("data shape",data.shape)
     return
 
 def extract_audio_features():
     for audio_file in glob.glob("audio_files/*.mp3"):
         print(f'Extracting audio features for {audio_file}')
         y, fs = sf.read(audio_file, dtype='float32')
+        print("len(y):", len(y))
+        print("duration (s):", len(y) / fs)
+        print('fs', fs)
+        if y.ndim == 2:  # (n_samples, n_channels)
+            y = y.T  # -> (n_channels, n_samples)
+            y = librosa.to_mono(y)
+        print('mono shape:', y.shape)
+        print('duration (s):', len(y) / fs)
+
         mfcc = lf.mfcc(y=y, n_mfcc=20)
-        print('mfcc', mfcc.shape)
+        # mfcc = mfcc.squeeze()
+        # mfcc = mfcc.T
+        print('mfcc', mfcc.shape, mfcc.ndim, type(mfcc))
         break
 
 
